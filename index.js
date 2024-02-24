f102636985b9a2a19ddffc42cb619f6a36212f5c
furak <furako.28@gmail.com>
2024-02-24 19:24:18 +0300

    limit 10

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get("/word",async (req,res)=>{
     })
   }
 
-  let words = await Word.find(filter).sort({createdAt:1}).limit(10);
+  let words = await Word.find(filter).sort({createdAt:1}).limit(1);
   
     
   res.json({success:{message:"All Words",data:words}});
